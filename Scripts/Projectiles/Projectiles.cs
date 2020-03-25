cbdbe7525b42b91db4300c397b26addf982ffadc
TheNiv <nivai2233@gmail.com>
2020-03-25 22:20:03 +0000

    Add files via upload

---
+++ Scripts/Projectiles/Projectiles.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectiles : MonoBehaviour
+{
+    public float speed;
+    private Vector2 screenBounds;
+    private Rigidbody2D rb;
+    
+    
+    void Start()
+    {   
+        rb = this.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(speed, 0);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        Debug.Log(screenBounds.x);
+        Debug.Log(screenBounds.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += transform.right * speed * Time.deltaTime;
+        
+        if(this.transform.position.x > screenBounds.x)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag =="Enemy")
+        {
+            Destroy(this.gameObject);
+            Destroy(collision.gameObject);
+        }
+            
+    }
+
+}
---
+++ Scripts/Projectiles/ShootProjectile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootProjectile : MonoBehaviour
+{
+    public GameObject projectile;
+    public int fireRate;  //the max number of projectiles that can be shot per second.
+    private float lastFired;  //the time the last projectile has been spawned. To make sure projectiles are not shot too fast.
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            if(Time.time -lastFired > 1f/fireRate)  //check if enough time has passed so it does not shoot too fast.
+            {
+
+                lastFired = Time.time;
+                GameObject p = Instantiate(projectile, this.transform.position, Quaternion.identity);  //spawn projectile
+                 
+
+            }
+            
+        }
+    }
+}
