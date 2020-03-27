3934aa8e7bbdb89e1bab22d456d075f731f1a662
TheNiv <nivai2233@gmail.com>
2020-03-27 21:11:59 +0000

    Update ShootProjectile.cs

--- Scripts/Projectiles/ShootProjectile.cs
+++ Scripts/Projectiles/ShootProjectile.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,6 +7,7 @@ public class ShootProjectile : MonoBehaviour
     public GameObject projectile;
     public int fireRate;  //the max number of projectiles that can be shot per second.
     private float lastFired;  //the time the last projectile has been spawned. To make sure projectiles are not shot too fast.
+    public static int virusesDestroyed = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,10 +19,13 @@ public class ShootProjectile : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Mouse0))
         {
-            if(Time.time -lastFired > 1f/fireRate)  //check if enough time has passed so it does not shoot too fast.
+            
+            if (Time.time -lastFired > 1f/fireRate)  //check if enough time has passed so it does not shoot too fast.
             {
 
-                lastFired = Time.time;
+                
+                
+                lastFired = Time.time;  //updating the lastFired variable
                 GameObject p = Instantiate(projectile, this.transform.position, Quaternion.identity);  //spawn projectile
                  
 
@@ -30,3 +34,4 @@ public class ShootProjectile : MonoBehaviour
         }
     }
 }
+
