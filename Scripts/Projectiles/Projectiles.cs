225d8a148191d0ef80bcb8c29d71d145070413f1
TheNiv <nivai2233@gmail.com>
2020-03-27 21:10:59 +0000

    Rotation added

--- Scripts/Projectiles/Projectiles.cs
+++ Scripts/Projectiles/Projectiles.cs
@@ -17,11 +17,14 @@ public class Projectiles : MonoBehaviour
         Vector3 mousePos = Input.mousePosition;  //getting the mouse position.
         mousePos = Camera.main.ScreenToWorldPoint(mousePos);  //trasnforming the mouse position to a point in the world.
         direction = new Vector2(mousePos.x, mousePos.y);  //getting the point direction.
-        
-        
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;  //the angle to rotate the projectile to.  
+        transform.rotation = Quaternion.Euler(0, 0, angle); // rotate the projectile to the target point.
+        direction.Normalize();
+        float distance = direction.magnitude;
+        direction = direction / distance ;
         Ray2D ray = new Ray2D(this.transform.position, direction);  //the ray of the projectile target
         rb.velocity =  speed * new Vector2(ray.direction.x, ray.direction.y); //setting the projectile movement direction
-        Debug.Log(ray.direction);
+        //Debug.Log(ray.direction);
 
         screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
@@ -41,6 +44,7 @@ public class Projectiles : MonoBehaviour
         {
             Destroy(this.gameObject);
             Destroy(collision.gameObject);
+            ShootProjectile.virusesDestroyed++;
         }
             
     }
